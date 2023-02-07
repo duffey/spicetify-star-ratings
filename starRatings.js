51ce96d2a3c9b2ec07aeb87c7e1ac67c85a797b7
Scott Duffey <scottrduffey@gmail.com>
2023-02-08 01:12:59 +0900

    Update version number in comments

--- starRatings.js
+++ starRatings.js
@@ -1,7 +1,7 @@
 // @ts-check
 // NAME: Star Ratings
 // AUTHOR: Scott Duffey
-// VERSION: 1.6.3
+// VERSION: 1.6.4
 // DESCRIPTION: Rate songs with stars and automatically save them to playlists
 
 /// <reference path='../globals.d.ts' />
