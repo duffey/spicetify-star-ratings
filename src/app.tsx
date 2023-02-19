ae0165dddd14121fb659625694905f5b1fa17987
Scott Duffey <scottrduffey@gmail.com>
2023-02-20 01:08:52 +0900

    Remove debug statement

--- src/app.tsx
+++ src/app.tsx
@@ -8,8 +8,6 @@ async function main() {
         await new Promise((resolve) => setTimeout(resolve, 100));
     }
 
-    console.log("hey");
-
     const fiveColumnGridCss = "[index] 16px [first] 4fr [var1] 2fr [var2] 1fr [last] minmax(120px,1fr)";
     const sixColumnGridCss = "[index] 16px [first] 6fr [var1] 4fr [var2] 3fr [var3] 2fr [last] minmax(120px,1fr)";
     const sevenColumnGridCss = "[index] 16px [first] 6fr [var1] 4fr [var2] 3fr [var3] minmax(120px,2fr) [var3] 2fr [last] minmax(120px,1fr)";
