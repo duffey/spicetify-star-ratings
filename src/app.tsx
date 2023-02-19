cfbcdf47e3a3e5e247a2989419a679bf752dbdfd
Scott Duffey <scottrduffey@gmail.com>
2023-02-20 01:35:26 +0900

    Move ratings functions to separate file

--- src/app.tsx
+++ src/app.tsx
@@ -2,6 +2,7 @@ import * as api from "./api";
 import { createStars } from "./stars";
 import { loadSettings, saveSettings, getSetting, setSetting } from "./settings";
 import { displaySettings } from "./settings-ui";
+import { getRatings, getRatedPlaylists } from "./ratings";
 
 async function main() {
     while (!Spicetify?.showNotification) {
@@ -11,8 +12,6 @@ async function main() {
     const fiveColumnGridCss = "[index] 16px [first] 4fr [var1] 2fr [var2] 1fr [last] minmax(120px,1fr)";
     const sixColumnGridCss = "[index] 16px [first] 6fr [var1] 4fr [var2] 3fr [var3] 2fr [last] minmax(120px,1fr)";
     const sevenColumnGridCss = "[index] 16px [first] 6fr [var1] 4fr [var2] 3fr [var3] minmax(120px,2fr) [var3] 2fr [last] minmax(120px,1fr)";
-
-    const RATINGS = ["0.0", "0.5", "1.0", "1.5", "2.0", "2.5", "3.0", "3.5", "4.0", "4.5", "5.0"];
 
     const waitForElement = (selector) => {
         return new Promise((resolve) => {
@@ -31,50 +30,6 @@ async function main() {
             });
         });
     };
-
-    function filterRatedPlaylists(playlists) {
-        const result = {};
-        for (const playlist of playlists.items) {
-            if (!RATINGS.includes(playlist.name)) continue;
-            result[playlist.name] = playlist;
-        }
-        return result;
-    }
-
-    async function getRatedPlaylists() {
-        let playlists = await api.getPlaylists();
-        const rated = playlists.items.find((playlist) => playlist.type === "folder" && playlist.name === "Rated");
-        if (!rated) {
-            return [[], null];
-        }
-        playlists = rated;
-        return [filterRatedPlaylists(playlists), rated.uri];
-    }
-
-    async function getRatings() {
-        const [playlists, ratedFolder] = await getRatedPlaylists();
-        const ratings = {};
-        for (const rating in playlists) {
-            const items = await api.getPlaylistItems(playlists[rating].uri);
-            for (const item of items) {
-                const uri = item.link;
-                if (!ratings[uri]) {
-                    ratings[uri] = rating;
-                } else {
-                    const m = parseFloat(rating);
-                    const n = parseFloat(ratings[uri]);
-                    const lower = m < n ? rating : ratings[uri];
-                    const higher = m > n ? rating : ratings[uri];
-                    ratings[uri] = higher;
-                    console.log(
-                        `Removing track ${item.name} with lower rating ${lower} and higher rating ${higher} from lower rated playlist ${playlists[lower].name}.`
-                    );
-                    await api.deleteTrackFromPlaylist(playlistUriToId(playlists[lower].uri), uri);
-                }
-            }
-        }
-        return [playlists, ratedFolder, ratings];
-    }
 
     function playlistUriToId(uri) {
         return uri.match(/spotify:playlist:(.*)/)[1];
